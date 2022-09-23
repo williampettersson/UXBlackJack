73ddc8e67b47b6ac18a1b159a25ba6369bd3ad34
williampettersson <william.c.pettersson@gmail.com>
2022-09-23 12:22:43 +0200

    added buttons to the game

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 console.log('hello!');
 
 var deck = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 ,12, 13],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 ,12, 13],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 ,12, 13],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 ,12, 13]
+    ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10 ,'J' ,'Q', 'K'],
+    ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10 ,'J' ,'Q', 'K'],
+    ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10 ,'J' ,'Q', 'K'],
+    ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10 ,'J' ,'Q', 'K']
 ]
 
 var colour = {
-    
+    Hjärter: '',
+    Ruter: '',
+    Klöver: '',
+    Spader: ''
 }
 
-console.table(deck);
-console.log(deck[3][8]);
+
+
+function build() {
+    console.table(deck);
+    console.log(deck[3][8]);
+}
+
+function startGame() {
+
+    const max = Math.cell(deck[0][0]);
+    const min = Math.floor(deck[3][12]);
+
+    console.log(randomCard(deck));
+
+    const divMain = document.getElementById('main');
+    const newGame = document.getElementById('start');
+    newGame.innerHTML = 'Start NEW game';
+    divMain.hidden = false;
+
+}
+
